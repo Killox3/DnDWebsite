9b246b49398c604e593b5593e5ad2830d2ccf34a
Mac Keane <is1111@live.com>
2014-09-24 14:17:15 +1200

    Trying to get strMod to work

--- main.js
+++ main.js
@@ -4,20 +4,28 @@ function rollD (x) {
   var rolled = 0;
   rolled = Math.ceil(Math.random() * x);
   return rolled;
-}
+};
 
 /* Ability Modifier Calculation Function 
   takes an ability score and returns the resulting ability modifier*/
 
-function abilityMod(x) {
-  var mod = 0;
-  mod = Math.floor((x-10)/2);
-  return mod;
+function total(){
+    var a = document.forms["ATTRIBUTES"]["str"].value;
+    var display=document.getElementById("strMod")
+    display.innerHTML=Math.floor((parseInt(a,10)-10)/2);
+    return false;
 }
 
-function isNumber(n) {
+
+/*var str = document.getElementsByName("STR")[0];
+var strMod = document.getElementByName("STRMOD")[0];
+function strMod() {
+  var calc = Math.floor((str.value-10)/2);
+};
+ str.onchange = strMod*/
+/* function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
-}
+} */
 
 document.querySelector("#D4").innerHTML = rollD(4);
 document.querySelector("#D6").innerHTML = rollD(6);
@@ -25,4 +33,4 @@ document.querySelector("#D8").innerHTML = rollD(8);
 document.querySelector("#D10").innerHTML = rollD(10);
 document.querySelector("#D12").innerHTML = rollD(12);
 document.querySelector("#D20").innerHTML = rollD(20);
-document.querySelector(placeholder="#strMod").innerHTML = abilityMod($("#str").val());
+//document.querySelector(placeholder="#strMod").innerHTML = abilityMod($("#str").val());
